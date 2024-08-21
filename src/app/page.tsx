baefcf9c89c1126f837d3c44ee19183ec690f364
Harshalvk <harshalvkhobragade@gmail.com>
2024-08-21 21:19:27 +0530

    ADD: Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import LatestPost from "@/components/home/LatestPost";
+import { MainNav } from "@/components/MainNav";
 
 export default function Home() {
   return (
     <>
-      <LatestPost/>
+      <MainNav />
+      <main>
+        <div>
+          <LatestPost />
+        </div>
+      </main>
     </>
   );
 }
