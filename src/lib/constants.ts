de075fdc6163e15dd2558fd2257a698c30fc592d
Harshalvk <harshalvkhobragade@gmail.com>
2024-08-26 22:47:42 +0530

    FIX: extract posts to contants file for clean code

---
+++ src/lib/constants.ts
@@ -0,0 +1,35 @@
+export const POSTS: { title: string; href: string; description: string }[] = [
+  {
+    title: "Learn React.js",
+    href: "/blog/react",
+    description: "Learn React.js & Next.js in simple and efficient way.",
+  },
+  {
+    title: "JavaScript",
+    href: "/blog/javascript",
+    description: "Learn what's are new in the javascript world.",
+  },
+  {
+    title: "CSS Flexbox",
+    href: "/blog/css-flexbox",
+    description:
+      "Master CSS Flexbox to create responsive and flexible layouts.",
+  },
+  {
+    title: "TypeScript Basics",
+    href: "/blog/typescript",
+    description:
+      "Get started with TypeScript and enhance your JavaScript coding skills.",
+  },
+  {
+    title: "Node.js Fundamentals",
+    href: "/blog/nodejs",
+    description: "Understand the core concepts of Node.js.",
+  },
+  {
+    title: "React Native",
+    href: "/blog/react-native",
+    description:
+      "Explore React Native for building cross-platform mobile applications efficiently.",
+  },
+];
