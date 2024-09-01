195f383d84a7059d4dcf1f818c10c5398e1caf9e
Harshalvk <harshalvkhobragade@gmail.com>
2024-09-01 23:25:27 +0530

    ADD: Breadcrumb component

---
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { formatDate, getBlogPosts } from "../../utils";
+import { notFound } from "next/navigation";
+import Header from "@/components/Header";
+import Container from "@/components/Container";
+import { BreadcrumbWithCustomSeparator } from "@/components/Breadcrumb";
+
+const page = ({ params }: { params: { category: string; slug: string } }) => {
+  let post = getBlogPosts().find((post) => post.slug === params.slug);
+
+  if (!post) {
+    notFound();
+  }
+  return (
+    <>
+      <Header>
+        <Container>
+          <BreadcrumbWithCustomSeparator
+            category={post.metadata.category}
+            slug={post.slug}
+          />
+          <h1 className="title font-semibold text-2xl tracking-tighter mt-4">
+            {post.metadata.title}
+          </h1>
+          <div className="flex justify-between items-center mt-2 mb-4 text-sm">
+            <p className="text-sm text-neutral-600 dark:text-neutral-400 mt-2">
+              {formatDate(post.metadata.publishedAt)}
+            </p>
+          </div>
+        </Container>
+      </Header>
+    </>
+  );
+};
+
+export default page;
